bcc02a34d009368f5c73c00add74954097185c2a
matty <35756280+murrty@users.noreply.github.com>
2018-03-14 01:34:16 -0500

    Add files via upload
    initial suspender form, includes suspend resume and process list
    
    contains some other stuff as well.

---
+++ frmSuspender.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace SuspensionTest {
+    partial class frmSuspender {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnSuspend = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.cbProcesses = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnSuspend
+            // 
+            this.btnSuspend.Location = new System.Drawing.Point(11, 37);
+            this.btnSuspend.Name = "btnSuspend";
+            this.btnSuspend.Size = new System.Drawing.Size(75, 23);
+            this.btnSuspend.TabIndex = 1;
+            this.btnSuspend.Text = "suspend";
+            this.btnSuspend.UseVisualStyleBackColor = true;
+            this.btnSuspend.Click += new System.EventHandler(this.btnSuspend_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.Location = new System.Drawing.Point(87, 37);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(75, 23);
+            this.btnResume.TabIndex = 2;
+            this.btnResume.Text = "resume";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // cbProcesses
+            // 
+            this.cbProcesses.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbProcesses.FormattingEnabled = true;
+            this.cbProcesses.Location = new System.Drawing.Point(11, 10);
+            this.cbProcesses.Name = "cbProcesses";
+            this.cbProcesses.Size = new System.Drawing.Size(151, 21);
+            this.cbProcesses.TabIndex = 3;
+            // 
+            // frmSuspender
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(172, 70);
+            this.Controls.Add(this.cbProcesses);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnSuspend);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(180, 100);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(180, 100);
+            this.Name = "frmSuspender";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "process suspender";
+            this.Load += new System.EventHandler(this.frmSuspender_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSuspend;
+        private System.Windows.Forms.Button btnResume;
+        private System.Windows.Forms.ComboBox cbProcesses;
+    }
+}
+
---
+++ frmSuspender.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuspensionTest {
+    public partial class frmSuspender : Form {
+        List<string> susProc = new List<string>();
+        public frmSuspender() {
+            InitializeComponent();
+        }
+
+        public enum ThreadAccess : int {
+            TERMINATE = (0x1),
+            SUSPEND_RESUME = (0x2),
+            GET_CONTEXT = (0x8),
+            SET_CONTEXT = (0x10),
+            SET_INFORMATION = (0x20),
+            QUERY_INFORMATION = (0x40),
+            SET_THREAD_TOKEN = (0x80),
+            IMPERSONATE = (0x100),
+            DIRECT_IMPERSONATION = (0x200)
+        }
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenThread(ThreadAccess dwDesriedAccess, bool bInheritHandle, int dwThreadId);
+        [DllImport("kernel32.dll")]
+        public static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        public static extern uint ResumeThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        public static extern bool CloseHandle(IntPtr hHandle);
+
+        private void sus(Process proc) {
+            foreach (ProcessThread t in proc.Threads) {
+                IntPtr th;
+                th = OpenThread(ThreadAccess.SUSPEND_RESUME, false, t.Id);
+                if (th != IntPtr.Zero) {
+                    SuspendThread(th);
+                    CloseHandle(th);
+                }
+            }
+        }
+
+        private void res(Process proc) {
+            foreach (ProcessThread t in proc.Threads) {
+                IntPtr th;
+                th = OpenThread(ThreadAccess.SUSPEND_RESUME, false, t.Id);
+                if (th != IntPtr.Zero) {
+                    ResumeThread(th);
+                    CloseHandle(th);
+                }
+            }
+        }
+
+        private void btnSuspend_Click(object sender, EventArgs e) {
+            string sproc = cbProcesses.GetItemText(cbProcesses.SelectedItem);
+            if (sproc.EndsWith(".exe"))
+                sproc = sproc.Substring(sproc.Length - 4);
+
+            if (susProc.Contains(sproc)) {
+                return;
+            }
+
+            foreach (Process proc in Process.GetProcessesByName(sproc)) {
+                sus(proc);
+            }
+            susProc.Add(sproc);
+        }
+
+        private void btnResume_Click(object sender, EventArgs e) {
+            string rproc = cbProcesses.GetItemText(cbProcesses.SelectedItem);
+            if (rproc.EndsWith(".exe"))
+                rproc = rproc.Substring(rproc.Length - 4);
+
+            foreach (Process proc in Process.GetProcessesByName(rproc)) {
+                res(proc);
+            }
+            susProc.Remove(rproc);
+        }
+
+        private void frmSuspender_Load(object sender, EventArgs e) {
+            foreach (Process Proc in Process.GetProcesses()) {
+                cbProcesses.Items.Add(Proc.ProcessName);
+            }
+            cbProcesses.Sorted = true;
+            cbProcesses.SelectedIndex = 0;
+        }
+    }
+}
